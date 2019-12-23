93aa065159aa2172fb04a8ae93f8de0f8ae95690
Aaditya Chaudhary <1chaudharaad@hdsb.ca>
2019-12-23 18:16:06 -0500

    added blank files for parks,users, and events, as well as a blank login view

---
+++ project-app/park_planner/lib/pages/Login.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+  Login({Key key}) : super(key: key);
+
+  @override
+  _LoginState createState() {
+    return _LoginState();
+  }
+}
+
+class _LoginState extends State<Login> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+}
